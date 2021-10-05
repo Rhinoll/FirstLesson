97867aed85f9effa9da880aab0b3628ca2851f66
Andrew <zenit.yfon@gmail.com>
2021-10-05 15:50:40 +0300

    I'm DONE TORT's!!!

--- src/task2/Programm.java
+++ src/task2/Programm.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class Programm {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double x = scan.nextDouble();
-        double y = scan.nextDouble();
+        int x = scan.nextInt();
 
-        if (x < 1 & y > 0 & (y > 2*x*x || y > 1 -x)) {
-            System.out.println("YES");
-        } else System.out.println("NO");
+        if (x == 0 || x % 100 == 11 || (x % 100 >= 10 & x % 100 <= 20)) {
+            System.out.println(x + " TOPTOB");
+        }
+        else if ((x == 1) || (x % 10 == 1 & x != 11)){
+            System.out.println(x + " TOPT");
+        }
+        else if ((x > 1 & x < 5) || (x > 20 & x < 100 & x % 10 > 1 & x % 10 < 5) ||(x > 100 & x % 10 > 1 & x % 10 < 5) ) {
+            System.out.println(x + " TOPTA");
+        }
+        else if (x > 4) {
+            System.out.println(x + " TOPTOB");
+        }
     }
 }
-//Тестируем комиты через гитхаб
