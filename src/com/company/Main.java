54b58bda6b26f3eaaac8949a3584cd5ddb60530f
Andrew <zenit.yfon@gmail.com>
2021-10-05 12:45:05 +0300

    Trying to do First Repository

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//System.out.println("Hello world!");
+    int x = 8;
+    long y = 10;
+
+    x = (int)y; //прямое переназначение типов
+
+    y = x;
+
+    double z = 7.8;
+    float v = 7.8f; //как обозначается float
+    char ch = 'a'; //char одинарными кавычками
+    String str = "Test";
+
+    int m = 3/2; //деление нацело
+    int g = 3%2; //остаток от деления
+
+    double q = Math.sin(x);
+
+
+
+    }
+}
---
+++ src/task2/Programm.java
@@ -0,0 +1,15 @@
+package task2;
+
+import java.util.Scanner;
+
+public class Programm {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double x = scan.nextDouble();
+        double y = scan.nextDouble();
+
+        if (x < 1 & y > 0 & (y > 2*x*x || y > 1 -x)) {
+            System.out.println("YES");
+        } else System.out.println("NO");
+    }
+}
